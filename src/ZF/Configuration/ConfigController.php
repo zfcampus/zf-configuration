3979d80c69ed7f54487a4afde61ddc0c0bf740a3
Matthew Weier O'Phinney <matthew@zend.com>
2013-08-05 15:11:52 -0500

    Switch usage of trees based on content-type
    - If content-type is application/vnd.zfcampus.v1.config, use trees

--- src/ZF/Configuration/ConfigController.php
+++ src/ZF/Configuration/ConfigController.php
@@ -19,15 +19,37 @@ class ConfigController extends AbstractActionController
     
     public function processAction()
     {
-        $request = $this->getRequest();
+        $request  = $this->getRequest();
+        $headers  = $request->getHeaders();
+        $useTrees = $this->shouldUseTrees($headers);
+
         switch ($request->getMethod()) {
             case $request::METHOD_GET:
-                return $this->config->fetch();
+                return $this->config->fetch($useTrees);
             case $request::METHOD_PATCH:
                 $params = $this->bodyParams();
-                return $this->config->patch($params);
+                return $this->config->patch($params, $useTrees);
             default:
                 return new ApiProblem(405, 'Only the methods GET and PATCH are allowed for this URI');
         }
+    }
+
+    protected function shouldUseTrees($headers)
+    {
+        if (!$headers->has('content-type')) {
+            return false;
+        }
+
+        $header      = $headers->has('content-type');
+        $value       = $header->getFieldValue();
+        $value       = explode(';', $value, 2);
+        $contentType = array_shift($value);
+        $contentType = strtolower($value);
+
+        if ($contentType === 'application/vnd.zfcampus.v1.config') {
+            return true;
+        }
+
+        return false;
     }
 }
