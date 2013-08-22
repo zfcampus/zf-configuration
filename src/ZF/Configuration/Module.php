62daa2d677cfb518bc335eef137b7710823c942e
Matthew Weier O'Phinney <matthew@zend.com>
2013-08-22 06:48:23 -0500

    Added factory for ModuleConfigController

--- src/ZF/Configuration/Module.php
+++ src/ZF/Configuration/Module.php
@@ -66,7 +66,11 @@ class Module
             'ZF\Configuration\ConfigController' => function ($controllers) {
                 $services = $controllers->getServiceLocator();
                 return new ConfigController($services->get('ZF\Configuration\ConfigResource'));
-            }
+            },
+            'ZF\Configuration\ModuleConfigController' => function ($controllers) {
+                $services = $controllers->getServiceLocator();
+                return new ConfigController($services->get('ZF\Configuration\ConfigResourceFactory'));
+            },
         ));
     }
 }
