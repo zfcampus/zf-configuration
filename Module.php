533c6cc04d7fb14226173c427e18ce1144e66ee7
Matthew Weier O'Phinney <matthew@zend.com>
2014-03-18 13:10:21 -0500

    Do not enable bracket syntax
    - For portability purposes (e.g., running the admin on 5.4, but deploying on
      5.3), legacy array syntax should *always* be used.

--- Module.php
+++ Module.php
@@ -47,10 +47,6 @@ class Module
                     }
                 }
                 $writer = new PhpArray();
-                if ($useShortArray && version_compare(PHP_VERSION, '5.4.0', '>=')) {
-                    $writer->setUseBracketArraySyntax(true);
-                }
-
                 return $writer;
             },
             'ZF\Configuration\ConfigResource' => function ($services) {
